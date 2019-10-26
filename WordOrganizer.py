cbcfe9998b95bcb0f4c718dc160cad49df249d6b
Aditya Mitra <adityamitra1089@gmail.com>
2019-10-26 12:07:28 -0700

    This is a code to alphabetize words.

---
+++ WordOrganizer.py
@@ -0,0 +1,15 @@
+# Code to organize words
+
+# Get the input
+words = input("Input some words:")
+
+# Split the input into strings
+WordString = words.split(',')
+
+# Sort the words
+WordString.sort()
+
+# Print the sorted words
+print(WordString)
+
+
