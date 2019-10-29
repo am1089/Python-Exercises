6bb843acbd7cad4e3f599c59215633de160765a4
Aditya Mitra <adityamitra1089@gmail.com>
2019-10-28 19:56:02 -0700

    Got it working to have an infinte number of inputs

--- ListOrganizer.py
+++ ListOrganizer.py
@@ -1,24 +1,24 @@
 # Code to sort multiple lists
-lists = []
+# input fields sperated by commas
+allip = []
+iplist = []
 
-print("Input a name, age, and height")
-input1 = input()
-i1 = input1.split(",")
+print("Input some names, ages, heights")
 
-print("Input a second name, age, and height")
-input2 = input()
-i2 = input2.split(",")
-
-print("Input a third name, age, and height")
-input3 = input()
-i3 = input3.split(",")
-
-# add all inputs to list as sublists
-lists.extend([i1, i2, i3])
-
-# this sorts by each index of each of the sublists
-lists.sort(key=lambda x: (x[0], x[1], x[2]))
+# Collect all inputs until an empty line is found. 
+while True:
+    ip = input()
+    if ip == "":
+        break
+    # Split into strings and then into integers
+    iplist = ip.split(',')
+    allip.append([iplist[0], int(iplist[1]), int(iplist[2])])
 
 
-print(lists)
+# lambda is creating a tuple which is given as a key then sorted.
+# To sort it in decending order make the x's negative Ex: x[0] -> -x[0]
+allip.sort(key=lambda x: (x[0], x[1], x[2]))
+
+
+print(allip)
 
