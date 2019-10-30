c4444fdac3a35493a6670887ea9e650f0ef29c92
Aditya Mitra <adityamitra1089@gmail.com>
2019-10-29 18:46:54 -0700

    Made a code that makes the fibonacci sequence using generators.

---
+++ FibonacciGenerator.py
@@ -0,0 +1,18 @@
+# Every yield adds a generator to the list
+def fibonacci(n):
+    a, b = 0, 1
+    yield a
+    for _ in range(n-1):
+        a, b = b, a+b
+        yield a
+    return
+
+
+print('Input a number')
+number = int(input())
+
+if number <= 0:
+    print('Please input a positive number')
+else:
+    print('Fibonacci Sequence:')
+    print(list(fibonacci(number)))
