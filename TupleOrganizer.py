dc39db4adc48604cc4051353625fedc1063f1b81
Aditya Mitra <adityamitra1089@gmail.com>
2019-11-04 17:57:19 -0800

    Input to PickEven is now a tuple

--- TupleOrganizer.py
+++ TupleOrganizer.py
@@ -1,15 +1,16 @@
 # Selects all even numbers from a tuple and outputs a tuple
 
-def organize(n):
+def PickEven(n):
+        # Converts tuple to list to find even numbers
         Ip = list(n)
-        # Yield even numbers from list n
+        # Yield even numbers from list Ip
         for i in range(len(Ip)):
                 if Ip[i]%2 == 0:
                         yield Ip[i]
 
 kbd = input("Enter a group of numbers:")
 
-# Turns list of strings into list of integers
-Input = [int(i) for i in list(kbd.split(','))]
+# Turns input strings into tuple of integers
+Input = tuple(int(i) for i in list(kbd.split(',')))
 
-print(tuple(organize(Input)))
+print("Output:", tuple(PickEven(Input)))
